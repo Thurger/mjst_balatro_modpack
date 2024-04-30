d4c354dde2ce45f988d3929a18c014651f8edeaf
matheo.pons <matheo.pons@epitech.eu>
2024-04-30 17:04:27 +0200

    [ADD] : Localization & Fast assets

---
+++ formula_one/localization/en-us.lua
@@ -0,0 +1,66 @@
+﻿return {
+    descriptions = {
+        Joker = {
+        },
+        Voucher = {
+        },
+        Tarot = {
+        },
+        Planet = {
+        },
+        Spectral = {
+        },
+        Edition = {
+        },
+        Enhanced = {
+            m_mjst_mod_formula_one_raced = {
+                name = "Raced Card",
+                text = {
+                    "{X:mult,C:white}X#1#{} Mult per finished Ante",
+                    "{C:inactive}(Currently {X:mult,C:white}X#2#{C:inactive} Mult)"
+                }
+            }
+        },
+        Stake = {
+        },
+        Tag = {
+        },
+        Blind = {
+        },
+        Back = {
+        },
+        Other = {
+        }
+    },
+    UI = {},
+    misc = {
+        suits_singular = {
+        },
+        suits_plural = {
+        },
+        blind_states = {
+        },
+        ranks = {
+        },
+        high_scores = {
+        },
+        achievement_names = {
+        },
+        achievement_descriptions = {
+        },
+        challenge_names = {
+        },
+        poker_hands = {
+        },
+        poker_hand_descriptions = {
+        },
+        labels = {
+        },
+        dictionary = {
+        },
+        v_dictionary = {
+        },
+        v_text = {
+        },
+    }
+}
